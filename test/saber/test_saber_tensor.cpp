1d9f101005cb73e9cb0dcc4054d704af5e78bbe5
guangzhixie <guangzhi.xie@berkeley.edu>
2018-08-14 13:40:48 +0800

    debug

--- test/saber/test_saber_tensor.cpp
+++ test/saber/test_saber_tensor.cpp
@@ -20,7 +20,7 @@ void tensor_constructor() {
     //std::cout << "targetD is __host_target? " << std::is_same<target_D, __host_target>::value << std::endl;;
     //std::cout << "targetD is target_H? " << std::is_same<target_D, target_H>::value << std::endl;;
     //std::cout << "flag_type? " << flag_type();
-    std::cout << "int, const int: " << std::is_same<int, const int>::value << std::endl;
+    std::cout << "targetD is __host_target? " << std::is_same<target_D, __host_target>::value << std::endl;
 
     typedef Tensor<TargetH> TensorH;
     typedef Tensor<TargetD> TensorD;
